edc5b2bf9e29a92f035406d8809c51031c254c48
Ki Yung Ahn <kyagrd@gmail.com>
2020-03-18 23:13:38 +0900

    update

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ HOSTPORT=8888
 
 if [ -z "$(ls -A ".snapshots" 2>/dev/null)" ]; then
 	docker run --rm -p $HOSTPORT:8888 $LAB \
-		-v "$PWD":/home/jovyan/pwd \
+		-v "$PWD":/home/jovyan/work \
 		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
 	       	crosscompass/ihaskell-notebook:$TAG
 else
 	docker run --rm -p $HOSTPORT:8888 $LAB \
-		-v "$PWD":/home/jovyan/pwd \
+		-v "$PWD":/home/jovyan/work \
 	       	-v "$PWD/.snapshots":/opt/stack/snapshots \
 	       	-v "$PWD/.stack-work":/opt/stack/global-project/.stack-work \
 		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
